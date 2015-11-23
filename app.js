45172dd043a31a8544ff19799d20dd8ae6e69f1e
Jessica Straw <jcstraw3@gmail.com>
2015-11-22 21:56:05 -0500

    Adds another way to do routes (currently commented out)

--- app.js
+++ app.js
@@ -6,5 +6,17 @@
 	$(document).ready(function(){
 		app.run('#/');
 	});
+
+	// $(document).ready(function(){
+	// 	var app = Sammy('#container', function(){
+	// 		this.get('#/', function(context){
+	// 			alert(context.verb);
+	// 		});
+	// 		this.get('#/page', function(context){
+	// 			alert('test');
+	// 		});
+	// 	});
+	// 	app.run('#/');
+	// });
 	
 })();
