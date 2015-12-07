5fb7fca14ee6004c84a624475d36a155f1e5407e
Jessica Straw <jcstraw3@gmail.com>
2015-12-07 18:38:41 -0500

    Removes commented out code

--- app.js
+++ app.js
@@ -6,30 +6,5 @@
 	$(document).ready(function(){
 		app.run('#/');
 	});
-
-	// $(document).ready(function(){
-	// 	var app = Sammy('#container', function(){
-	// 		this.use(Sammy.Template);
-
-	// 		this.get('#/', function(context){
-	// 			context.render('/view/main.template', {
-	// 				'myVariable': 'Header',
-	// 				'newVariable': 'new',
-	// 			}, function(view){
-	// 				$('#container').html(view);
-	// 			});
-	// 		});
-
-	// 		this.get('#/page', function(context){
-	// 			context.render('/view/page.template', {
-	// 				'myVariable': 'Header2',
-	// 			}, function(view){
-	// 				$('#container').html(view);
-	// 			});
-	// 		});
-
-	// 	});
-	// 	app.run('#/');
-	// });
 	
 })();
